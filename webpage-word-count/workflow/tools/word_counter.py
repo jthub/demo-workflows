b40bf33ef4a2ff116ce0e6d1cd6b1c87735dde02
Junjun Zhang <junjun.ca@gmail.com>
2017-10-13 00:57:30 -0400

    improvement, remove dependency on bs4

--- webpage-word-count/workflow/tools/word_counter.py
+++ webpage-word-count/workflow/tools/word_counter.py
@@ -2,7 +2,6 @@
 
 import sys
 import re
-import bs4
 from utils import get_task_dict, save_output_json
 
 task_dict = get_task_dict(sys.argv[1])
@@ -13,11 +12,10 @@ file_ = task_dict.get('input').get('file')
 with open(file_, 'r') as f:
     webpage_html = f.read()
 
-soup = bs4.BeautifulSoup(webpage_html, 'html.parser')
-results = soup.body.find_all(string=re.compile('.*{0}.*'.format(word)), recursive=True)
+match_pattern = re.findall(r'\b{0}\b'.format(word), webpage_html)
 
 output_json = {
-    'count': len(results),
+    'count': len(match_pattern),
     'word': word
 }
 
